4dbdbed45349bb8575acd4ce272629b3901a354e
= <=>
2020-04-21 14:40:56 +0700

    thêm close

--- TestCSDLBuoi1/Form1.cs
+++ TestCSDLBuoi1/Form1.cs
@@ -27,5 +27,10 @@ namespace TestCSDLBuoi1
         {
             Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
